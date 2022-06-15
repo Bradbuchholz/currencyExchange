fcdc38279770298d1537d7906cf59fc581d9b1fc
Bradbuchholz <bradbuchholz@gmail.com>
2022-06-15 12:44:30 -0700

    update index.html, index.js, exchangeService.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
-// entry point for project. 
-// user interface logic.
-// Import JS/CSS files here.
-// ie: import { PeanutButter } from './peanut-butter.js'
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExchangeService from 'src/js/exchangeService.js'
+
+function clearFields() {
+  $('#exchCurr').val("");
+  $('.showErrors').text("");
+  $('.showExchange').text("");
+}
+
+$(document).ready(function() {
+  $('#exchange').click(function() {
+    let amount = $('#USD').val();
+    clearFields();
+    let promise = ExchangeService.getRate(amount);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showExchange').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+  });
+});
